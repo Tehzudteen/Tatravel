f5c4f0b319d1aaf783811c3c6f1f384f0dd57e5b
Tehzudteen <luckyteh0083@gmail.com>
2024-11-19 00:44:10 +0700

    final

--- Backend/routes/travel.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createTravel, getTravelDetails } = require('../controllers/TravelController');
-const router = express.Router();
-
-router.post('/', createTravel);
-router.get('/:travelId/details', getTravelDetails);
-
-module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -7,8 +7,7 @@ require("dotenv").config();
 const chatRoute = require("./routes/chat");
 const connectDB = require("./config/db"); 
 const authRoutes = require('./routes/auth');
-const travelRoutes = require('./routes/travel');
-const authMiddleware = require('./middleware/auth');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,8 +22,6 @@ app.use(cors());
 // Routes
 app.use("/api", chatRoute);
 app.use('/auth', authRoutes);
-app.use('/travels', authMiddleware, travelRoutes);
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
